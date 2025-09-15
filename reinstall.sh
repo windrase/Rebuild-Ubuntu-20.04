01ad4e238b5a3084d6a2a569435ca73da2410262
WindraSe <pemaingamez123@gmail.com>
2025-09-15 17:27:29 +0700

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -8,7 +8,7 @@ confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
 # confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 # 默认密码
-DEFAULT_PASSWORD=123@@@
+DEFAULT_PASSWORD=Windrase123@
 
 # 用于判断 reinstall.sh 和 trans.sh 是否兼容
 SCRIPT_VERSION=4BACD833-A585-23BA-6CBB-9AA4E08E0003
